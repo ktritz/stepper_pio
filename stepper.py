16ab06533fccedf8802dfe640aa9516b11034e2b
ktritz <ktritz@gmail.com>
2022-06-18 12:39:29 -0400

    changed delay calculation, decimated step size

--- stepper.py
+++ stepper.py
@@ -1,8 +1,9 @@
 import cos_calc
-from math import sqrt
-
 import numpy as np
 import matplotlib.pyplot as plt
+from math import sqrt, ceil
+import array
+
 
 PI = 3.1415926535
 PI2 = PI * PI
@@ -10,16 +11,26 @@ MAX = 1000
 AT = PI2 / MAX
 SQAT = PI / MAX
 
+with open("cos_table.py") as file:
+    num = int(file.readline().strip())
+    table = array.array("L", [0] * num)
+    for i in range(num):
+        table[i] = int(file.readline().strip())
+
+TABLE_SCALE = 2 * PI / 2 ** 16
+
 
 class Stepper:
     PIO_FREQ = 1e6
-    BIT_LIMIT = 16
+    DELAY_BIT_LIMIT = 16
+    STEP_BIT_LIMIT = 32 - DELAY_BIT_LIMIT
 
     def __init__(self, max_velocity=1000, acceleration=1.0):
         self.max_velocity = max_velocity  # velocity in Hz (1 / delay)
         self.acceleration = acceleration  # time to max_velocity in sec.
-        self.micro_steps = 1
-        self._table = cos_calc.sq_table(MAX + 1)
+        self.micro_steps = 8
+        self._table = table
+        self._repeat = 1
 
     def _gen_table(self, num=MAX + 1):
         table = (cos_calc.sq_table(num) / 2 / PI * 2 ** 16).astype(np.uint16)
@@ -33,21 +44,11 @@ class Stepper:
     def _unique(self, delays):
         u_delays = sorted(set(delays))[::-1]
         u_counts = [0] * len(u_delays)
-        for d in delays:
+        for d in delays[0 : self._repeat]:
             u_counts[u_delays.index(d)] += 1
+        for d in delays[self._repeat :]:
+            u_counts[u_delays.index(d)] += self._repeat
         return u_delays, u_counts
-
-    def _solve(self, sqppos):
-        # will return the interpolated time (0..2*PI) that corresponds to a given distance sqrt(0 .. PI*PI)
-        # according to inverse lookup table
-        sqt = self._table
-        if sqppos >= PI:
-            return sqt[MAX - 1]
-        n = int(sqppos / SQAT)
-        dp = sqppos - n * SQAT
-        if dp == 0:
-            return sqt[n]
-        return (sqt[n + 1] - sqt[n]) * dp / SQAT + sqt[n]
 
     def _accel_steps(self, velocity=None):
         # number of (micro) steps to reach velocity in acceleration time
@@ -55,31 +56,55 @@ class Stepper:
             velocity = self.max_velocity
         kt = 0.1 * self.acceleration / 0.63 * self.micro_steps
         max_delay = 1 / velocity / kt * 2
-        steps = (self._table[-1] - self._table[-2]) / max_delay * 1000
+        steps = (self._table[-1] - self._table[-2]) / max_delay * 1000 * TABLE_SCALE
         return int(steps)
 
     def _calc_velocity(self, steps):
         # reduce max_velocity if steps < _accel_steps(max_velocity)
         kt = 0.1 * self.acceleration / 0.63 * self.micro_steps
-        max_delay = (self._table[-1] - self._table[-2]) / steps * 1000
+        max_delay = (self._table[-1] - self._table[-2]) / steps * 1000 * TABLE_SCALE
         velocity = 1 / max_delay / kt * 2
         return velocity
 
+    def _delay(self, i, steps):
+        # will return the interpolated time (0..2*PI) that corresponds to a given distance sqrt(0 .. PI*PI)
+        # according to inverse lookup table
+        sqt = self._table
+        sqppos = PI * sqrt(i / steps)
+
+        # approximate sqrt((i+1)/steps) - sqrt(i/steps) to estimate delay at step i
+        if i == 0:
+            dp = PI * sqrt(1 / steps)
+        else:
+            dp = PI * (sqrt(1 / 4 / steps / i) - sqrt(1 / 64 / steps / i) / i)
+
+        if sqppos >= PI:
+            return (sqt[-1] - sqrt[-2]) * dp / SQAT
+        n = int(sqppos / SQAT)
+        return (sqt[n + 1] - sqt[n]) * dp / SQAT
+
     def _accel_delays(self, velocity=None):
-        limit = 2 ** self.BIT_LIMIT - 1
+        delay_limit = 2 ** self.DELAY_BIT_LIMIT - 1
         if velocity is None:
             velocity = self.max_velocity
         # generate list of (delays in us, # of pulses) for cos-based S-curve
-        steps = self._accel_steps(velocity=velocity) + 1
-        kt = 0.1 * self.PIO_FREQ * self.acceleration / 0.63 * steps / (steps - 1)
-        d = [self._solve(sqrt(PI2 * i / (steps))) * kt for i in range(steps)]
-        delays = [min(int(d[i + 1] - d[i]), limit) for i in range(len(d[:-1]))]
+        steps = self._accel_steps(velocity=velocity)
+        self._repeat = ceil(steps / 2000)
+        kt = 0.1 * self.PIO_FREQ * self.acceleration / 0.63
+        delays = [
+            min(int(self._delay(i, steps) * kt * TABLE_SCALE), delay_limit)
+            for i in range(0, self._repeat)
+        ]
+        delays += [
+            min(int(self._delay(i, steps) * kt * TABLE_SCALE), delay_limit)
+            for i in range(self._repeat, steps, self._repeat)
+        ]
         return list(zip(*self._unique(delays)))
 
     def gen_delays(self, steps):
         velocity = self.max_velocity
         steps = int(steps) * self.micro_steps
-        limit = 2 ** self.BIT_LIMIT - 1
+        step_limit = 2 ** self.STEP_BIT_LIMIT - 1
         acc_steps = self._accel_steps()
         half_steps = steps // 2
         if half_steps < acc_steps:
@@ -92,10 +117,10 @@ class Stepper:
         c_delay = acc_delays[-1][0]
         c_steps = acc_delays[-1][1]
         c_steps = 2 * c_steps + cruise_steps
-        while c_steps > limit:
-            acc_delays[-1] = (c_delay, limit)
+        while c_steps > step_limit:
+            acc_delays[-1] = (c_delay, step_limit)
             acc_delays.append((c_delay, 0))
-            c_steps -= limit
+            c_steps -= step_limit
         acc_delays[-1] = (c_delay, c_steps)
         acc_delays.extend(dec_delays)
         return acc_delays
