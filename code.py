f7dfea302b774585842f10d8e5cf10efe9fb1d3d
ktritz <ktritz@gmail.com>
2022-06-22 16:29:34 -0400

    tweaks

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import stepper
 import board
 
 s = stepper.Stepper(
-    board.D0, limit_pins=(board.D7, board.D7), count_pin=board.D2, dir_pin=board.D6
+    board.D6, limit_pins=(board.D0, board.D1), dir_pin=board.D4, encoder_pis=(board.D2, board.D3)
 )
 s.dir_active = "HIGH"
 s._setup_sm()
--- lib/stepper.py
+++ lib/stepper.py
@@ -3,7 +3,7 @@ import rotaryio
 
 
 class Stepper(StepperPIO):
-    ENC_TICKS_PER_MM = 500
+    ENC_TICKS_PER_MM = 50
 
     def __init__(self, step_pin, encoder_pins=None, limit_pins=None, **kwargs):
 
@@ -11,9 +11,8 @@ class Stepper(StepperPIO):
             self.encoder = rotaryio.IncrementalEncoder(*encoder_pins)
         else:
             self.encoder = None
-        
+
         self.limit_pins = limit_pins
-    
         super().__init__(step_pin, **kwargs)
 
     @property
@@ -21,7 +20,12 @@ class Stepper(StepperPIO):
         if self.encoder:
             return self.encoder.position / self.ENC_TICKS_PER_MM
         else:
-            return super().position
+            return self.steps / self.STEPS_PER_REV * self.MM_PER_REV
+
+    @position.setter
+    def position(self, value):
+        delta_mm = value - self.position
+        self.steps = delta_mm / self.MM_PER_REV * self.STEPS_PER_REV
 
     def _set_dir(self, direction):
         self.direction = direction
@@ -30,4 +34,3 @@ class Stepper(StepperPIO):
             self._setup_sm()
         super()._set_dir(direction)
 
-        
